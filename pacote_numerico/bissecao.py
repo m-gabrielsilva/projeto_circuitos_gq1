eafc378a5ab9e5634cec5e84dd5fbfa9411fc195
M10Legend2 <moisesflopesf@gmail.com>
2026-09-19 11:23:44 -0300

    Adicionado os notebooks, criação de ambiente virtual e pyproject.toml com o gerenciador "uv", adição e modificação de histórico nas funções, amostragem da tabela de histórico e gráfico de comparação de convergência

--- pacote_numerico/bissecao.py
+++ pacote_numerico/bissecao.py
@@ -1,4 +1,7 @@
+import pandas as pd
+
 def metodo_bissecao(f, a, b, tol, max_iter):
+    historico = []
   
     if f(a) * f(b) >= 0:
         raise ValueError("O intervalo inicial é inválido. f(a) e f(b) devem ter sinais opostos.")
@@ -8,7 +11,9 @@ def metodo_bissecao(f, a, b, tol, max_iter):
     while (b - a) >= tol and k < max_iter:
 
         x = (a + b) / 2.0 
-        
+        erro = abs(b-a) / 2
+        historico.append({'Iteração': k, 'x_novo': x, 'Erro': erro})
+
         if f(x) == 0:
             a = x
             b = x
@@ -23,7 +28,7 @@ def metodo_bissecao(f, a, b, tol, max_iter):
         
     raiz_aprox = (a + b) / 2.0
     
-    return raiz_aprox, k
+    return pd.DataFrame(historico), raiz_aprox, k
 
 
 # =====================================================================
--- pacote_numerico/newton_raphson.py
+++ pacote_numerico/newton_raphson.py
@@ -1,7 +1,9 @@
+import pandas as pd
+
 def newton_raphson(f, df, x0, eps1=1e-6, eps2=1e-6, kmax=100):
 
     x = x0
-    hist = []
+    historico = []
 
     for k in range(1, kmax + 1):
         derivada = df(x)
@@ -9,11 +11,11 @@ def newton_raphson(f, df, x0, eps1=1e-6, eps2=1e-6, kmax=100):
             raise ZeroDivisionError(f"Derivada nula na iteração {k} para x = {x}")
 
         x_novo = x - f(x) / derivada
-        hist.append((k, x_novo, f(x_novo), abs(x_novo - x)))
+        historico.append({'Iteração':k, 'X_novo':x_novo, "f(x)":abs(f(x_novo)), 'Erro': abs(x_novo - x)})
 
         if abs(f(x_novo)) < eps1 or abs(x_novo - x) < eps2:
-            return x_novo, hist
+            return pd.DataFrame(historico), x_novo, k
 
         x = x_novo
 
-    return x, hist
+    return pd.DataFrame(historico), x_novo, k
